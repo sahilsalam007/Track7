a2fb8b9e09fb2c9c43aa33727baf8f9a50cd4aec
sahilsalam007 <salamsahil474@gmail.com>
2025-07-30 14:28:57 +0530

    changed google callback url

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ const env =require("dotenv").config();
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"http://localhost:1122/auth/google/callback"
+    callbackURL:process.env.GOOGLE_CALLBACK_URL
 },
 async (accessToken,refreshToken,profile,done)=>{
     try{
