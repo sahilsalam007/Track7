cc53c62baef3350a026e56505fe412e8d845688e
sahilsalam007 <salamsahil474@gmail.com>
2025-07-30 14:15:52 +0530

    fixed the incorrect directory name

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use((req,res,next)=>{
     
 app.set("view engine","ejs");
 app.set("views",[path.join(__dirname,"views/user"),path.join(__dirname,"views/admin")])
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"Public")));
 
 app.use("/",userRouter)
 app.use("/admin",adminRouter);
